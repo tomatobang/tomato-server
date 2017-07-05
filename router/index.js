d0ea0ad651dd2532b52d60a247379ca7938a53ae
kobepeng <pengkobe123@gmail.com>
2017-07-05 19:30:08 +0800

    add tomatotoday

--- router/index.js
+++ router/index.js
@@ -4,7 +4,7 @@ let models = require("../model/mongo");
 
 module.exports.init = function(router) {
   /**
-   * 邮箱验证
+   * 邮箱验证 getTodayTomatos
   */
   router.post("/email_username/verify", async function(ctx, next) {
     let usermodel = models.user;
@@ -30,6 +30,31 @@ module.exports.init = function(router) {
         success: true,
         msg: "邮箱可用！"
       };
+    }
+  });
+
+  router.get("/filter/tomatotoday", async function(ctx, next) {
+    let tomatomodel = models.tomato;
+    let datenow = new Date();
+    let date = datenow.getFullYear() +"-" + (datenow.getMonth()+1) + "-" + datenow.getDate(); // ,"$lte":new Date()
+    // 按用户筛选
+    let conditions = { startTime: { $gte:new Date(date).toISOString() } };
+    if (ctx.request.currentUser) {
+       conditions.userid = ctx.request.currentUser.username;
+    }else{
+      ctx.status = 200;
+      ctx.body = [];
+    }
+    
+    let ret = await tomatomodel 
+      .find(conditions) 
+      .exec();
+    if (ret.length) {
+      ctx.status = 200;
+      ctx.body = ret;
+    }else{
+      ctx.status = 200;
+      ctx.body = [];
     }
   });
 };
