6fb774bebc3fb0f1136bb0d7634a1094b0a7c793
kobepeng <pengkobe123@gmail.com>
2017-07-20 11:13:54 +0800

    init jpush

---
+++ service/jpush.js
@@ -0,0 +1,15 @@
+const JPush = require("jpush-sdk")
+let client = JPush.buildClient('your appKey', 'your masterSecret')
+
+//easy push
+client.push().setPlatform(JPush.ALL)
+    .setAudience(JPush.ALL)
+    .setNotification('Hi, JPush', JPush.ios('ios alert', 'happy', 5))
+    .send(function(err, res) {
+        if (err) {
+            console.log(err.message)
+        } else {
+            console.log('Sendno: ' + res.sendno)
+            console.log('Msg_id: ' + res.msg_id)
+        }
+    });
