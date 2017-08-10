bcd5ba2dd8f8e3676647746104fe58feb2d62547
kobepeng <pengkobe123@gmail.com>
2017-08-10 16:10:01 +0800

    add rongcloudSDK to finish the request(succeed)

--- utils/rongyun.js
+++ utils/rongyun.js
@@ -1,10 +1,25 @@
-const RongAPI = require('co-rongcloud-api');
-const api = new RongAPI(appid, appsecret);
+// const RongAPI = require("co-rongcloud-api");
+// const api = new RongAPI("", "");
 
-const dissmissgroup = (userid,groupid)=>{
-    let ret = await api.groupDismiss(userid,groupid);
-}
+var rongcloudSDK = require( 'rongcloud-sdk' );
+rongcloudSDK.init("appkey", "appsecret");
 
-module.export = {
-    dissmissgroup
-}
+const dissmissgroup = async (userid, groupid) => {
+	// console.log("a am here!");
+	// try {
+	// 	let ret = await api.groupDismiss(userid, groupid);
+	// 	console.log("a am here!", ret);
+	// 	return ret;
+	// } catch (e) {
+	// 	console.log("dissmissgroup err!", e);
+	// }
+	await 100;	
+	rongcloudSDK.group.dismiss(userid,groupid,'json',function(err, resultText){
+		console.log("err",err);
+		console.log("resultText",resultText);
+	});
+};
+
+module.exports = {
+	dissmissgroup
+};
