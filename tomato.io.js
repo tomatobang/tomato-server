e8f29839f35e80b6eaa99c48ca682cc48f10faa3
kobepeng <pengkobe123@gmail.com>
2017-08-21 20:55:53 +0800

    fill socket code to blank

--- tomato.io.js
+++ tomato.io.js
@@ -12,40 +12,86 @@ let cahce = {};
 // 断开连接，则删除cache
 
 let dataModels = require("./model/mongo.js");
-let userModel = dataModels.user;
-let user_hash = {};
-let socket_hash = {};
+let tomatomodel = dataModels.tomatomodel;
+let toamato_hash = {};
 /**
  * 视频聊天中转服务器
 */
 module.exports = io => {
-	io.of("/tomato").on("connection", function(socket) {
-
-        /**
-         * 开始
+	io.of("/tomato").on("connection", socket => {
+		/**
+         * 加载已有番茄钟
          */
-		socket.on("add_tomato", function(userid, headImg) {
-            //socket.emit("msg", { msg: "you sent : " + msg });
-            // 如果已有番茄钟，那么直接覆盖
-		});
-
-        /**
-         * 番茄钟中断
-         */
-		socket.on("break_tomato", function(userid, headImg) {
+		socket.on("load_tomato", (userid, endname) => {
+            let hash = toamato_hash[userid];
+            if (hash && hash.tomato){
+                io
+                .of("/tomato")
+                .to(socket.id)
+                .emit("load_tomato_succeed", hash.tomato);
+            }
         });
 
-        /**
+		/**
+         * 开始
+         */
+		socket.on("start_tomato", (userid, endname, tomato) => {
+			let hash = toamato_hash[userid];
+			hash.end = endname;
+			let socketList = hash.socketList;
+			hash.tomato = tomato;
+			for (let so of socketList) {
+				if (so != socket.id) {
+					io
+						.of("/tomato")
+						.to(so)
+						.emit("other_end_start_tomato", tomato);
+				}
+			}
+		});
+
+		/**
+         * 番茄钟中断
+         */
+		socket.on("break_tomato", async (userid, endname, tomato) => {
+			let hash = toamato_hash[userid];
+			hash.end = endname;
+			let socketList = hash.socketList;
+			hash.tomato = tomato;
+
+			// 保存当前番茄钟
+			const result = await model.create(tomato);
+			if (result) {
+				for (let so of socketList) {
+					if (so != socket.id) {
+						io
+							.of("/tomato")
+							.to(so)
+							.emit("other_end_break_tomato", tomato);
+					}
+				}
+			}
+		});
+
+		/**
          * 番茄钟完成
          */
-		socket.on("finish", function(userid, headImg) {});
+		// socket.on("finish", (userid, tomato) => {
+
+        // });
 
 		/**
 		 * 离线处理
 		 */
-		socket.on("disconnect", function() {
-            // 由用户离开造成的中断，且没有其它终端在线，则直接算为终端番茄钟
-
-        });
+		socket.on("disconnect", () => {
+			let hash = toamato_hash[userid];
+			let socketList = hash.socketList;
+			if (result) {
+				var index = socketList.indexOf(socket.id);
+                if (index > -1) {
+                    socketList.splice(index, 1);
+                }
+			}
+		});
 	});
 };
