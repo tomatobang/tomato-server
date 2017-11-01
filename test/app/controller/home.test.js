e8b1d692bc73b7a43071bb9693ce8b482514e635
kobepeng <pengkobe123@gmail.com>
2017-11-01 20:32:34 +0800

    begin to run test for controllers

--- test/app/controller/home.test.js
+++ test/app/controller/home.test.js
@@ -1,4 +1,25 @@
 const { app, mock, assert } = require('egg-mock/bootstrap');
 describe('test/controller/home.test.js', () => {
+    let token = "";
+    // 先进行登录
+    it('should login succeed',async () => {
+        let loginRes = await app.httpRequest()
+            .post('/api/login')
+            .type('form')
+            .send({
+                username: 'pengyi',
+                password: 'pengyi',
+            });
+        assert(loginRes.status === 200);
+        token = loginRes.body.token;
+    });
 
+    it('should status 200 and get the body', () => {
+        // 对 app 发起 `GET /` 请求
+        return app.httpRequest()
+            .get('/')
+            .set('Authorization', token)
+            .expect(200)
+            .expect('Hello, this is TomatoBang Index Page');
+    });
 });
--- test/app/middleware/jwt.test.js
+++ test/app/middleware/jwt.test.js
@@ -3,28 +3,28 @@
 const assert = require('assert');
 const mock = require('egg-mock');
 describe('test/app/middleware/jwt.test.js', () => {
-  let app;
-  before(() => {
-    // 创建当前应用的 app 实例
-    app = mock.app();
-    // 等待 app 启动成功，才能执行测试用例
-    return app.ready();
-  });
-  afterEach(mock.restore);
-  it('should return token required', () => {
-    return app.httpRequest()
-      .get('/')
-      .expect(200)
-      .expect('{"status":"fail","description":"Token not found"}'); 
-  });
+    let app;
+    before(() => {
+        // 创建当前应用的 app 实例
+        app = mock.app();
+        // 等待 app 启动成功，才能执行测试用例
+        return app.ready();
+    });
+    afterEach(mock.restore);
+    it('should return token required', () => {
+        return app.httpRequest()
+            .get('/')
+            .expect(200)
+            .expect('{"status":"fail","description":"Token not found"}');
+    });
 
-  it('should return token invalid', () => {
-    return app.httpRequest()
-      .get('/')
-      .set('Authorization', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBlbmd5aSIsInRpbWVzdGFtcCI6MTUwOTUxODMwNzI0NSwiaWF0IjoxNTA5NTE4MzA3LCJleHAiOjE1MDk2MDQ3MDd9.vlPzj_m4Q8A01NobJh5DRLIvnd-ohfC1ZEWyUxvi674")
-      .expect(200)
-      .expect('{"status":"fail","description":"Token invalid"}');
-  });
+    it('should return token invalid', () => {
+        return app.httpRequest()
+            .get('/')
+            .set('Authorization', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InBlbmd5aSIsInRpbWVzdGFtcCI6MTUwOTUxODMwNzI0NSwiaWF0IjoxNTA5NTE4MzA3LCJleHAiOjE1MDk2MDQ3MDd9.vlPzj_m4Q8A01NobJh5DRLIvnd-ohfC1ZEWyUxvi674")
+            .expect(200)
+            .expect({ status: "fail", description: "Token invalid" });
+    });
 
 });
 
