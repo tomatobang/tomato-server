d8b50c48bb6bb952882d2d2d8b5021c7eeebbab5
kobepeng <pengkobe123@gmail.com>
2018-01-30 10:41:37 +0800

    feat: init framework.js

---
+++ test/index.test.js
@@ -0,0 +1,26 @@
+'use strict';
+// TODO:
+// 测试 framework 用
+
+// const mock = require('egg-mock');
+// describe('test/index.test.js', () => {
+//   let app;
+//   before(() => {
+//     app = mock.app({
+//       // 转换成 test/fixtures/apps/example
+//       baseDir: 'apps/example',
+//       // 重要：配置 framework
+//       framework: true,
+//     });
+//     return app.ready();
+//   });
+
+//   after(() => app.close());
+//   afterEach(mock.restore);
+
+//   it('should success', () => {
+//     return app.httpRequest()
+//       .get('/')
+//       .expect(200);
+//   });
+// });
