7c5676205fe00b32d9134fe67c1da10565952ff1
kobepeng <pengkobe123@gmail.com>
2018-07-04 13:48:04 +0800

    feat: support io auth

--- app/io/controller/chat.ts
+++ app/io/controller/chat.ts
@@ -186,7 +186,7 @@ module.exports = (app: Application) => {
         request_time: new Date().valueOf(),
         state: 1,
       };
-      // TODO: 不能重复发送好友请求
+      // TODO: 不能重复发送好友请求( 目前使用逐渐验证 )
       await ctx.service.userFriend.create(user_friend);
 
       this.notify(to, 'receive_friend_request', from);
