a4033bfc2f7a14b31da4e16a164b679daa6fa5e4
kobepeng <pengkobe123@gmail.com>
2018-07-19 20:43:07 +0800

    feat: add rsmq-worker

--- app/util/rsmq.ts
+++ app/util/rsmq.ts
@@ -1,9 +1,10 @@
 /**
  * remq d.ts see： https://github.com/smrchy/rsmq/blob/master/index.d.ts
  */
-
+import { Application } from 'egg';
 import * as RedisSMQ from 'rsmq';
-let rsmq = new RedisSMQ({ host: '127.0.0.1', port: 6379, ns: 'rsmq' });
+const RSMQWorker = require('rsmq-worker');
+const rsmq = new RedisSMQ({ host: '127.0.0.1', port: 6379, ns: 'rsmq' });
 
 export const mqUtil = {
   create: (name: string): Promise<any> => {
@@ -58,5 +59,57 @@ export const mqUtil = {
         }
       );
     });
+  },
+
+  listen: (name, app: Application) => {
+    var worker = new RSMQWorker(name);
+
+    worker.on('message', async (msg, next, id) => {
+      console.log('Message id : ' + id);
+      console.log(msg);
+      /**
+       * msg:
+       * {
+       *    evtName,
+       *    message,
+       * }
+       */
+      await notify(name, msg.evtName, msg.messagge);
+
+      next();
+    });
+
+    worker.on('error', (err, msg) => {
+      console.log('ERROR', err, msg.id);
+    });
+    worker.on('exceeded', msg => {
+      console.log('EXCEEDED', msg.id);
+    });
+    worker.on('timeout', msg => {
+      console.log('TIMEOUT', msg.id, msg.rc);
+    });
+
+    worker.start();
+
+    /**
+     * send message
+     * @param {string} userid userid
+     * @param {string} evtName socket.io event name
+     * @param {string} message message
+     */
+    async function notify(userid, evtName, message) {
+      const redisChatService = app.util.redis.redisChatService;
+      const loginEnds = await redisChatService.findUserSocket(app, userid);
+      if (loginEnds && loginEnds.length > 0) {
+        for (const end of loginEnds) {
+          if (end) {
+            await app.io
+              .of('/chat')
+              .to(end)
+              .emit(evtName, message);
+          }
+        }
+      }
+    }
   },
 };
