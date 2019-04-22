1b570946090f33f86e638cda6306a43600a0e73a
kobepeng <pengkobe123@gmail.com>
2019-04-22 21:45:49 +0800

    feat: add regular todo apis

--- app/controller/todoRegular.ts
+++ app/controller/todoRegular.ts
@@ -6,7 +6,45 @@ import { todoRegularValidationRule } from '../validate/todoRegular';
 export default class TodoRegularController extends BaseController {
     constructor(ctx) {
         super(ctx);
-        this.service = ctx.service.todo;
+        this.service = ctx.service.todoRegular;
         this.validateRule = todoRegularValidationRule;
+    }
+
+    async list() {
+        const { ctx } = this;
+        let conditions: any;
+        conditions = {};
+        const query = ctx.request.query;
+        ctx.logger.info('ctx.request：', ctx.request['currentUser']);
+        // filter with logged userinfo
+        if (ctx.request['currentUser']) {
+            conditions.userid = ctx.request['currentUser']._id;
+        }
+        if (query.conditions) {
+            conditions = JSON.parse(query.conditions);
+        }
+        const result = await this.service.findAll(query, conditions);
+        ctx.body = result;
+        ctx.status = 200;
+    }
+
+    async create() {
+        const { ctx, app } = this;
+        // filter with logged userinfo
+        if (ctx.request['currentUser']) {
+            ctx.request.body.userid = ctx.request['currentUser']._id;
+        }
+        if (this.validateRule) {
+            const invalid = app['validator'].validate(
+                this.validateRule,
+                ctx.request.body
+            );
+            if (invalid) {
+                ctx.throw(400);
+            }
+        }
+        const result = await this.service.create(ctx.request.body);
+        ctx.status = 200;
+        ctx.body = result;
     }
 }
--- app/service/todoRegular.ts
+++ app/service/todoRegular.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 import BaseService from './base';
-export default class TodoRecordService extends BaseService {
+export default class TodoRegularService extends BaseService {
     constructor(ctx) {
         super(ctx);
         this.model = this.ctx.model.TodoRegular;
